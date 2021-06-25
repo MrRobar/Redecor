05bc564f0f6b42281aafce9bf0f00449bffe4966
Valeriy <mr.robariii@gmail.com>
2021-06-25 13:19:47 +0200

    ScriptableIntValue script created
    Allows to create SO with Int

---
+++ Redecor2D&3D/Assets/Scripts/ScriptableValues/ScriptableIntValue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ScriptableValues
+{
+
+    [CreateAssetMenu(fileName = "New Int", menuName = "Values/Int")]
+    public class ScriptableIntValue : ScriptableObject
+    {
+
+        public int data;
+    }
+}
