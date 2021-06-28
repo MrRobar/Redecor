5e83d0c561ee4a8e0d78939123e8c38ef20b60ae
Valeriy <mr.robariii@gmail.com>
2021-06-28 12:12:55 +0200

    SelectThroughMaterials script created
    To chose category from material

---
+++ Redecor2D&3D/Assets/Scripts/UI/SelectTroughMaterials.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectTroughMaterials : MonoBehaviour
+{
+
+    [SerializeField]
+    private int _categoryZonesCount;
+
+    [SerializeField]
+    private RectTransform _contentRect;
+
+    [SerializeField]
+    private Vector2[] _zonesPositions;
+
+    private int _selectedZoneID;
+
+    private void Awake()
+    {
+        _zonesPositions = new Vector2[_categoryZonesCount];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float nearestPos = float.MaxValue;
+        for (int i = 0; i < _categoryZonesCount; i++)
+        {
+            float distance = Mathf.Abs(_contentRect.anchoredPosition.x - _zonesPositions[i].x);
+            if (distance < nearestPos)
+            {
+                nearestPos = distance;
+                _selectedZoneID = i;
+                Debug.Log("Current: " + i);
+            }
+        }
+    }
+}
