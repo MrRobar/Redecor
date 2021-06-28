06540cb44317251a7b6755059db1544224ca3a9f
Valeriy <mr.robariii@gmail.com>
2021-06-28 12:12:04 +0200

    ScriptableVector2 script created
    keeps Vector2

---
+++ Redecor2D&3D/Assets/Scripts/ScriptableValues/ScriptableVector2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ScriptableValues
+{
+
+    [CreateAssetMenu(fileName = "New Vector2", menuName = "Values/New Vector2")]
+    public class ScriptableVector2 : ScriptableObject
+    {
+
+        public Vector2 data;
+    }
+}
+
