7c1c2d0791cae6dae04ec08e1d063a88b3c14189
Valeriy <mr.robariii@gmail.com>
2021-06-29 11:26:32 +0200

    ResourcesSpawner script created
    Spawns all stuff to the Materials panel

---
+++ Redecor2D&3D/Assets/Scripts/Managers/ResourcesSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Game.Managers
+{
+    public class ResourcesSpawner : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Transform _materialsKeeperPrefab;
+
+        [SerializeField]
+        private int _rows;
+
+        [SerializeField]
+        private int _columns;
+
+        private void Awake()
+        {
+            for (int i = 0; i < _columns; i++)
+            {
+                for (int j = 0; j < _rows; j++)
+                {
+                    
+                }
+            }    
+        }
+    }
+}
