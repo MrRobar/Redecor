57326ccd71a291b7ce96db67e6ef99576e60618f
Valeriy <mr.robariii@gmail.com>
2021-06-29 14:34:27 +0200

    ButtonName script created
    Keeps id of a button

---
+++ Redecor2D&3D/Assets/Scripts/UI/ButtonName.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using ScriptableValues;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class ButtonName : MonoBehaviour
+    {
+
+        [SerializeField]
+        private ScriptableIntValue _selectedID;
+
+        [SerializeField]
+        private int _id;
+
+        [SerializeField]
+        private Button _thisButton;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public void SetID()
+        {
+            _selectedID.data = _id;
+        }
+    }
+}
