0929050c33e959f285f725d651947ef4e2b43a86
Valeriy <mr.robariii@gmail.com>
2021-07-01 16:31:01 +0200

    ScriptableBool script created
    Allows to create a SO bool

---
+++ Redecor2D&3D/Assets/Scripts/ScriptableValues/ScriptableBool.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ScriptableValues
+{
+
+    [CreateAssetMenu(fileName = "New Bool", menuName = "Values/Bool")]
+    public class ScriptableBool : ScriptableObject
+    {
+
+        public bool data;
+    }
+}
