2f276e4e235853c570153e6b6b8a79ee2ce24374
Valeriy <mr.robariii@gmail.com>
2021-07-02 12:00:19 +0200

    ResourcesManager script updated
    Added EditNameState, SetProperName methods

--- Redecor2D&3D/Assets/Scripts/Managers/ResourcesManager.cs
+++ Redecor2D&3D/Assets/Scripts/Managers/ResourcesManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Events;
 using ScriptableValues;
 using Game.UI;
+using TMPro;
 
 namespace Game.Managers
 {
@@ -39,6 +40,9 @@ namespace Game.Managers
         [SerializeField]
         private RectTransform _materialsContent;
 
+        [SerializeField]
+        private RectTransform _empty;
+
         private Vector2[] _namesPositions;
         private Vector2[] _keepersPositions;
 
@@ -50,6 +54,9 @@ namespace Game.Managers
         
         [SerializeField]
         private int _snapSpeed;
+
+        [SerializeField]
+        private int _namesOffset = 80;
 
         private void Awake()
         {
@@ -69,25 +76,26 @@ namespace Game.Managers
                 }
 
                 keeper.localPosition = new Vector2(_keepersList[i - 1].localPosition.x + _materialsKeeperPrefab.GetComponent<RectTransform>().sizeDelta.x, _keepersList[i].localPosition.y);
-                name.localPosition = new Vector2(_namesList[i - 1].localPosition.x + _namePrefab.GetComponent<RectTransform>().sizeDelta.x, _namesList[i].localPosition.y);
+                name.localPosition = new Vector2(_namesList[i - 1].localPosition.x + _namePrefab.GetComponent<RectTransform>().sizeDelta.x + _namesOffset, _namesList[i].localPosition.y);
                 name.GetComponent<ButtonName>().ID = i;
                 _namesPositions[i] = -_namesList[i].transform.localPosition;
                 _keepersPositions[i] = -_keepersList[i].transform.localPosition;
             }
-
-            SetDefaultCategory();
+            Instantiate(_empty, _namesParent);
         }
 
         private void OnEnable()
         {
             _lateUpdateEventListener.OnEventHappened += SetNearestID;
             _lateUpdateEventListener.OnEventHappened += CheckForEqualIndex;
+            _lateUpdateEventListener.OnEventHappened += EditNameState;
         }
 
         private void OnDisable()
         {
             _lateUpdateEventListener.OnEventHappened -= SetNearestID;
             _lateUpdateEventListener.OnEventHappened -= CheckForEqualIndex;
+            _lateUpdateEventListener.OnEventHappened -= EditNameState;
         }
 
         private void SetNearestID() // Попробовать сравнивать Х < 270(половина NamesScroll)
@@ -110,9 +118,16 @@ namespace Game.Managers
             {
                 if(i == _selectedID.data)
                 {
-                    SetProperPack(i - 1);
+                    SetProperName(i);
+                    SetProperPack(i);
                 }
             }
+        }
+
+        private void SetProperName(int index)
+        {
+            _namesContentVector.x = Mathf.SmoothStep(_namesContent.anchoredPosition.x, -(index * 200), _snapSpeed * Time.deltaTime);
+            _namesContent.anchoredPosition = _namesContentVector;
         }
 
         private void SetProperPack(int index)
@@ -125,6 +140,20 @@ namespace Game.Managers
         {
             _namesContent.anchoredPosition = Vector2.zero;
             _materialsContent.anchoredPosition = Vector2.zero;
+        }
+
+        private void EditNameState()
+        {
+            for (int i = 0; i < _columns; i++)
+            {
+                _namesList[i].transform.GetChild(0).gameObject.SetActive(false);
+                _namesList[i].transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(0.4f, 0.4f, 0.4f);
+                if (i == _selectedID.data)
+                {
+                    _namesList[i].transform.GetChild(0).gameObject.SetActive(true);
+                    _namesList[i].transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(0f, 0f, 0f);
+                }
+            }
         }
     }
 }
