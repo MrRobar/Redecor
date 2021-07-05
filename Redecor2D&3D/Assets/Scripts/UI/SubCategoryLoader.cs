83bb0a5812ac8b8400c3040b273ccf576fd2dc8d
Valeriy <mr.robariii@gmail.com>
2021-07-05 14:29:52 +0200

    A bug occured

---
+++ Redecor2D&3D/Assets/Scripts/UI/SubCategoryLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableValues;
+
+namespace Game.Managers
+{
+
+    public class SubCategoryLoader : MonoBehaviour
+    {
+
+        [SerializeField]
+        private List<ScriptableCellInfo> _infoToLoad = new List<ScriptableCellInfo>();
+
+        [SerializeField]
+        private Transform _materialPrefab;
+
+        [SerializeField]
+        private Transform _parent;
+
+        private void OnEnable()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Instantiate(_materialPrefab, _parent);
+            }
+        }
+    }
+}
