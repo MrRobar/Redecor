89f791c24b8d4b71028ae554ab4ef8498a7e04f3
Valeriy <mr.robariii@gmail.com>
2021-07-05 13:34:46 +0200

    ScrollSetter script created
    Sets scroll bool

---
+++ Redecor2D&3D/Assets/Scripts/Managers/ScrollSetter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Events;
+using ScriptableValues;
+
+namespace Game.Managers
+{
+
+    public class ScrollSetter : MonoBehaviour
+    {
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        [SerializeField]
+        private ScriptableBool _isScrolling;
+
+
+        private void OnEnable()
+        {
+            _updateEventListener.OnEventHappened += SetScroll;
+        }
+
+        private void OnDisable()
+        {
+            _updateEventListener.OnEventHappened -= SetScroll;
+        }
+
+        private void SetScroll()
+        {
+            _isScrolling.data = Input.GetMouseButton(0);
+        }
+    }
+}
