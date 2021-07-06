5dc594ab7a03f0b2c8471fb7f84c7f72ee0a6465
Valeriy <mr.robariii@gmail.com>
2021-07-06 15:22:19 +0200

    ScriptableCategory script created
    Contains info about category name, subcategories amount, list of SubCategory class

---
+++ Redecor2D&3D/Assets/Scripts/ScriptableValues/ScriptableCategory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableValues
+{
+
+    [CreateAssetMenu(fileName = "New Category", menuName = "Data/Category")]
+    public class ScriptableCategory : ScriptableObject
+    {
+
+        public string categoryName;
+
+        public int subCategoriesAmount;
+
+        public List<SubCategory> subCategories;
+    }
+
+    [System.Serializable]
+    public class SubCategory
+    {
+        public string subCategoryName;
+        public List<ScriptableCellInfo> infoToLoad;
+    }
+}
