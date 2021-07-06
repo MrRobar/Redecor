53dbb8b50955eed6da8d82c459d4b606b11bd289
Valeriy <mr.robariii@gmail.com>
2021-07-06 15:24:19 +0200

    ButtonName script updated
    Added SetData method
    Added link to CellInfo

--- Redecor2D&3D/Assets/Scripts/UI/ButtonName.cs
+++ Redecor2D&3D/Assets/Scripts/UI/ButtonName.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using ScriptableValues;
 using UnityEngine.UI;
+using TMPro;
 
 namespace Game.UI
 {
@@ -11,10 +13,16 @@ namespace Game.UI
         private ScriptableIntValue _selectedID;
 
         [SerializeField]
+        private ScriptableCellInfo _cellInfo;
+
+        [SerializeField]
         private int _id;
 
         [SerializeField]
         private Button _thisButton;
+
+        [SerializeField]
+        private TextMeshProUGUI _nameText;
 
         public int ID
         {
@@ -22,9 +30,26 @@ namespace Game.UI
             set { _id = value; }
         }
 
+        public ScriptableCellInfo CellInfo
+        {
+            get { return _cellInfo; }
+            set { _cellInfo = value; }
+        }
+
+        private void OnEnable()
+        {
+            //StartCoroutine(SetData());
+        }
+
         public void SetID()
         {
             _selectedID.data = _id;
+        }
+
+        private IEnumerator SetData()
+        {
+            yield return new WaitForSeconds(0.001f);
+            _nameText.text = _cellInfo.categoryName;
         }
     }
 }
