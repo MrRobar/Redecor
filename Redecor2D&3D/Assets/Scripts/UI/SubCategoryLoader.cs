6b83bb67abb0380d7d20e65a686828695852b9e0
Valeriy <mr.robariii@gmail.com>
2021-07-06 15:28:10 +0200

    SubCategoryLoader script updated
    Added proper data initialization

--- Redecor2D&3D/Assets/Scripts/UI/SubCategoryLoader.cs
+++ Redecor2D&3D/Assets/Scripts/UI/SubCategoryLoader.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using ScriptableValues;
+using Events;
 
 namespace Game.UI
 {
@@ -13,16 +13,36 @@ namespace Game.UI
         private List<ScriptableCellInfo> _infoToLoad = new List<ScriptableCellInfo>();
 
         [SerializeField]
+        private EventListener _spawnedCategoriesEventListener;
+
+        [SerializeField]
         private Transform _materialPrefab;
 
         [SerializeField]
         private Transform _parent;
 
+        public List<ScriptableCellInfo> InfoToLoad
+        {
+            get { return _infoToLoad; }
+            set { _infoToLoad = value; }
+        }
+
         private void OnEnable()
+        {
+            _spawnedCategoriesEventListener.OnEventHappened += InitializeData;
+        }
+
+        private void OnDisable()
+        {
+            _spawnedCategoriesEventListener.OnEventHappened -= InitializeData;
+        }
+
+        private void InitializeData()
         {
             for (int i = 0; i < _infoToLoad.Count; i++)
             {
                 var material = Instantiate(_materialPrefab, _parent);
+                material.GetComponent<MaterialVariant>().ThisInfo = _infoToLoad[i];
             }
         }
     }
