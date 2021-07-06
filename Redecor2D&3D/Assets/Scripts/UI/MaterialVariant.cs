bd6c1d3e5452f1fde396ad0c9b30500ab34f5d81
Valeriy <mr.robariii@gmail.com>
2021-07-06 15:26:32 +0200

    MaterialVariant script created
    Keeps info about object to change, ScriptableCellInfo.
    Has methods: SetSpriteToObject, Ienumerator LoadData

---
+++ Redecor2D&3D/Assets/Scripts/UI/MaterialVariant.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using ScriptableValues;
+using TMPro;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+
+    public class MaterialVariant : MonoBehaviour
+    {
+
+        [SerializeField]
+        private ScriptableSpriteRenderer _objectToChange;
+
+        [SerializeField]
+        private ScriptableCellInfo _thisInfo;
+
+        public ScriptableCellInfo ThisInfo
+        {
+            get { return _thisInfo; }
+            set { _thisInfo = value; }
+        }
+
+        private void OnEnable()
+        {
+            StartCoroutine(LoadData());
+        }
+
+        public void SetSpriteToObject()
+        {
+            _objectToChange.data.sprite = _thisInfo.spriteToSet;
+            Debug.Log("Sprite set");
+        }
+
+        IEnumerator LoadData()
+        {
+            yield return new WaitForSeconds(0.001f);
+            transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = _thisInfo.matName;
+            transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = _thisInfo.cost.ToString();
+            transform.GetChild(0).GetComponent<Image>().sprite = _thisInfo.spriteToShow;
+        }
+    }
+}
