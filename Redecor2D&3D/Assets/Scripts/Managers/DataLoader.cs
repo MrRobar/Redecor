f9007e58e8362bbccc207a658b96fa84ecb538e0
Valeriy <mr.robariii@gmail.com>
2021-07-06 15:19:27 +0200

    DataLoader script created
    Contains lists with categories

---
+++ Redecor2D&3D/Assets/Scripts/Managers/DataLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableValues;
+
+namespace Game.Managers
+{
+
+    public class DataLoader : MonoBehaviour
+    {
+
+        public List<string> categoriesNames = new List<string>();
+
+        public List<ScriptableCategory> categories = new List<ScriptableCategory>();
+    }
+}
