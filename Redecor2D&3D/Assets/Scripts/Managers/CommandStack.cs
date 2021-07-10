2c2b8d2c72d430f1dacc6a76d4dc744556e9fb81
Valeriy <mr.robariii@gmail.com>
2021-07-10 17:49:13 +0200

    CommandStack script created
    Keeps abstacrt realizations of Undo/Redo/Process commands

---
+++ Redecor2D&3D/Assets/Scripts/Managers/CommandStack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Game.Interfaces;
+
+namespace Game.Managers
+{
+
+    public class CommandStack
+    {
+
+        public Stack<ICommand> _commandHistory = new Stack<ICommand>();
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+            _commandHistory.Push(command);
+        }
+
+        public void UndoLastCommand()
+        {
+            if(_commandHistory.Count <= 0)
+            {
+                return;
+            }
+            _commandHistory.Pop().Undo();
+        }
+
+        public void RedoLastCommand()
+        {
+            if(_commandHistory.Count <= 0)
+            {
+                return;
+            }
+            _commandHistory.Pop().Redo();
+        }
+    }
+}
