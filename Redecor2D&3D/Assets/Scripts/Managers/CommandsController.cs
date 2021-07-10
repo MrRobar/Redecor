a27d991f3450daf6d5bb8e20e383a2d5a83e4fe0
Valeriy <mr.robariii@gmail.com>
2021-07-10 17:50:38 +0200

    CommandsController script created
    Keeps Monobex realizations

---
+++ Redecor2D&3D/Assets/Scripts/Managers/CommandsController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using ScriptableValues;
+using Game.Commands;
+using Events;
+
+namespace Game.Managers
+{
+
+    public class CommandsController : MonoBehaviour
+    {
+
+        [SerializeField]
+        private EventListener _processDataEventListener;
+
+        [SerializeField]
+        private EventListener _undoEventListener;
+
+        [SerializeField]
+        private EventListener _redoEventListener;
+
+        [SerializeField]
+        private ScriptableSpriteRenderer _rendererToChange;
+
+        [SerializeField]
+        private ScriptableSpriteValue _spriteToSet;
+
+        [SerializeField]
+        ScriptableSpriteValue _defaultSprite;
+
+        private CommandStack _commandStack = new CommandStack();
+
+        private void OnEnable()
+        {
+            _processDataEventListener.OnEventHappened += SetTexture;
+            _undoEventListener.OnEventHappened += UndoCommand;
+            _redoEventListener.OnEventHappened += RedoCommand;
+        }
+
+        private void OnDisable()
+        {
+            _processDataEventListener.OnEventHappened -= SetTexture;
+            _undoEventListener.OnEventHappened -= UndoCommand;
+            _redoEventListener.OnEventHappened -= RedoCommand;
+        }
+
+        private void SetTexture()
+        {
+            _commandStack.ExecuteCommand(new SetTextureCommand(_rendererToChange.data, _spriteToSet.data, _defaultSprite.data));
+        }
+
+        private void UndoCommand()
+        {
+            _commandStack.UndoLastCommand();
+            Debug.Log(_commandStack._commandHistory.Count);
+        }
+
+        private void RedoCommand()
+        {
+            _commandStack.RedoLastCommand();
+            Debug.Log(_commandStack._commandHistory.Count);
+        }
+    }
+}
