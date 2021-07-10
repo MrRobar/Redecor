32498d42ef847fd01ef53389d38597d88786e46b
Valeriy <mr.robariii@gmail.com>
2021-07-10 17:47:35 +0200

    SetTextureCommand script created
    Command for setting texture

---
+++ Redecor2D&3D/Assets/Scripts/Commands/SetTextureCommand.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Game.Interfaces;
+
+namespace Game.Commands
+{
+    public class SetTextureCommand : ICommand
+    {
+
+        private SpriteRenderer _spriteRendererToChange;
+        private Sprite _originalSprite;
+        private Sprite _newSprite;
+
+        public SetTextureCommand(SpriteRenderer renderer, Sprite spriteToSet, Sprite defaultSprite)
+        {
+            _originalSprite = defaultSprite;
+            _spriteRendererToChange = renderer;
+            _newSprite = spriteToSet;
+        }
+
+        public void Execute()
+        {
+            _spriteRendererToChange.sprite = _newSprite;
+        }
+
+        public void Undo()
+        {
+            _spriteRendererToChange.sprite = _originalSprite;
+        }
+
+        public void Redo()
+        {
+            //Debug.Log(_newSprite.name);
+            //_spriteRendererToChange.sprite = _newSprite;
+            Execute();
+        }
+    }
+}
