6bf7bce4bd6cd2b3482a2e0e97e6698cbdbb6c7c
Valeriy <mr.robariii@gmail.com>
2021-07-10 17:51:35 +0200

    ScriptableSpriteValue script created
    To keep info on current sprite

---
+++ Redecor2D&3D/Assets/Scripts/ScriptableValues/ScriptableSpriteValue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ScriptableValues
+{
+
+    [CreateAssetMenu(menuName = "Values/Sprite", fileName = "New Sprite")]
+    public class ScriptableSpriteValue : ScriptableObject
+    {
+
+        public Sprite data;
+    }
+}
